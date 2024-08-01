bda0b0b6acfb2a3208bf4f63ae58bdab7bb2b62e
Nekifor <neksmui21@mail.ru>
2024-08-01 14:52:33 +0300

    fix animation time

--- src/features/slider/index.tsx
+++ src/features/slider/index.tsx
@@ -108,7 +108,7 @@ export const Slider = ({ cards }: { cards: cinemaData[] }) => {
           {
             transform: `translate(${nextPercentage}%)`,
           },
-          { duration: 1200, fill: 'forwards' },
+          windowWidth > 768 ? { duration: 1200, fill: 'forwards' } : { duration: 600, fill: 'forwards' },
         );
 
         for (const image of trackRef.current.getElementsByClassName(styles.image)) {
@@ -116,12 +116,12 @@ export const Slider = ({ cards }: { cards: cinemaData[] }) => {
             {
               objectPosition: `${100 + nextPercentage}% center`,
             },
-            { duration: 1200, fill: 'forwards' },
+            windowWidth > 768 ? { duration: 1200, fill: 'forwards' } : { duration: 600, fill: 'forwards' },
           );
         }
       }
     },
-    [isDragging, startX, prevPercentage, maxPercentage],
+    [isDragging, startX, prevPercentage, maxPercentage, windowWidth],
   );
 
   useEffect(() => {
